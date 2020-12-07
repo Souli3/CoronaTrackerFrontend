1536cdc6a6df21deb3760d0aca00b3651d46dbee
abdulhamid <57312937+ali1996-sy@users.noreply.github.com>
2020-12-08 00:25:35 +0100

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,102 +1,79 @@
-/* In a template literal, the ` (backtick), \ (backslash), and $ (dollar sign) characters should be 
-escaped using the escape character \ if they are to be included in their template value. 
-By default, all escape sequences in a template literal are ignored.*/
-import { getUserSessionData, setUserSessionData } from "../utils/session.js";
-import { RedirectUrl } from "./Router.js";
-import Navbar from "./Navbar.js";
-import { API_URL } from "../utils/server.js";
-
-let loginPage = `
-<div class="sidenav">
-         <div class="login-main-text">
-            <h1>CoronaTracker</h1><br><br>
-            <h2>Login Page</h2>
-            <p>Veuillez vous connecter ou vous inscrire pour acceder au site.</p><br><br><br><br><br><br><br>
-            <p><i>Ce site est un type de forum utilise pour traquer les cas de covid en Belgique...</i><p>
-            
-            
-         </div>
-      </div>
-      <div class="main">
-         <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-               <form>
-                  <div class="form-group">
-                     <label>Email</label>
-                      <input class="form-control" id="email" type="text" name="email" placeholder="Enter your email" required="" pattern="^\\w+([.-]?\\w+)*@\\w+([\.-]?\\w+)*(\\.\\w{2,4})+\$" />
-                  </div>
-                  <div class="form-group">
-                     <label>Password</label>
-                     <input class="form-control" id="password" type="password" name="password" placeholder="Enter your password" required="" pattern=".*[A-Z]+.*" />
-                  </div>
-                  <button type="submit" id="login" class="btn btn-secondary">Login</button>
-                  <button type="submit" id="register" class="btn btn-black">Register</button>
-                  <div class="alert alert-danger mt-2 d-none" id="messageBoard"></div>
-               </form>
-            </div>
-        </div>
-
-      </div>`;
-
-const LoginPage = () => {
-  let page = document.querySelector("#page");
-  
-  page.innerHTML = loginPage;
-
-  let loginForm = document.querySelector("form");
-  const user = getUserSessionData();
+let navBar = document.querySelector("#navBar");
+import {getUserSessionData} from "../utils/session.js";
+// destructuring assignment
+const Navbar = () => {
+  let navbar;
+  let user = getUserSessionData();    
   if (user) {
-    // re-render the navbar for the authenticated user
-    Navbar();
-    RedirectUrl("/");
-  } else loginForm.addEventListener("submit", onLogin);
+    navbar = `<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+    <a class="navbar-brand" 
+    >Corona Tracker</a>
+    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" 
+          >Accueil <span class="sr-only">(current)</span></a>
+        </li>
+        
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle"  id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Parametre</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" 
+            >Mon Compte</a>
+            <a class="dropdown-item" 
+            >Mes states</a>
+            <a class="dropdown-item" 
+            >?</a>
+          </div>
+        </li>
+      </ul>
+     
+      
+    </div>
+  </nav>`;
+  } else {//pas connecter 2 eme cas en bas
+    navbar = `<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+    <a class="navbar-brand" 
+    >Corona Tracker</a>
+    <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" data-uri="/">Accueil <span class="sr-only"></span></a>
+        </li>
+        
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle"  id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Parametre</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item"  >Mon Compte</a>
+            <a class="nav-item nav-link" href="#">DeleteAccount</a>
+
+            <a class="dropdown-item" >Mes states</a>
+          </div>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link"  data-uri="/login">Login <span class="sr-only"></span></a>
+      </li>
+      </ul>
+      <li class="nav-item active">
+      <a class="nav-link"  
+      >Login <span class="sr-only"></span></a>
+    </li>
+      
+    </div>
+  </nav>`;
+  }
+
+  return (navBar.innerHTML = navbar);
 };
 
-const onLogin = (e) => {
-  e.preventDefault();
-  let email = document.getElementById("email");
-  let password = document.getElementById("password");
-
-  let user = {
-    email: document.getElementById("email").value,
-    password: document.getElementById("password").value,
-  };
-
-  fetch(API_URL + "users/login", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    body: JSON.stringify(user), // body data type must match "Content-Type" header
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (!response.ok)
-        throw new Error(
-          "Error code : " + response.status + " : " + response.statusText
-        );
-      return response.json();
-    })
-    .then((data) => onUserLogin(data))
-    .catch((err) => onError(err));
-};
-
-const onUserLogin = (userData) => {
-  console.log("onUserLogin:", userData);
-  const user = { ...userData, isAutenticated: true };
-  setUserSessionData(user);
-  // re-render the navbar for the authenticated user
-  Navbar();
-  RedirectUrl("/");
-};
-
-const onError = (err) => {
-  let messageBoard = document.querySelector("#messageBoard");
-  let errorMessage = "";
-  if (err.message.includes("401")) errorMessage = "Wrong username or password.";
-  else errorMessage = err.message;
-  messageBoard.innerText = errorMessage;
-  // show the messageBoard div (add relevant Bootstrap class)
-  messageBoard.classList.add("d-block");
-};
-
-export default LoginPage;
+export default Navbar;
