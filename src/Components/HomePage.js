b0da992a18572b8ac0fc6f82f278949c18b2b4eb
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-12-07 22:56:32 +0100

    stat

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,10 +1,12 @@
 import Quote from "./Quote.js";
-
+import creatBorad, { createBoard } from "./Dashboard.js";
 let page = document.querySelector("#page");
-
 const HomePage = () => {    
   page.innerHTML = `<div id="quote"></div>`;
-  Quote(); 
+    Quote(); 
+   // console.log(document.getElementById('board').innerText)
+    if(!document.getElementById('board'))
+    creatBorad();
 };
 
 export default HomePage;
