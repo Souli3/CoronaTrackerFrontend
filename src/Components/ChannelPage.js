4b6ceb0b29e14aa59687cc930b7e6dbb328c348b
ali789 <ABDULHAMID.ALI.996@gmail.com>
2020-12-10 21:02:53 +0100

    conversations

--- src/Components/ChannelPage.js
+++ src/Components/ChannelPage.js
@@ -1,5 +1,7 @@
 import { API_URL } from "../utils/server";
 import UpdateChannel from "./UpdateChannel.js";
+import insertChat from"./ConversationPage.js"
+import { getUserSessionData } from "../utils/session";
 var html=(id,title,sujet,date,etat,region,user)=>{
   return   `
   <main role="main"  class="container channel p-5 border border-primary">
@@ -16,7 +18,16 @@ var html=(id,title,sujet,date,etat,region,user)=>{
 <form>
 <button type="submit" class="btn btn-primary fermer" >fermer</button>
 </form>
-</main>`
+                
+</main>
+<ul id="message"></ul>
+<div class="msj-rta macro">                        
+                    <div class="text text-r" style="background:whitesmoke !important">
+                        <input id="text-message" placeholder="Type a message"/>
+                    </div> 
+                    <button data-id="${id}" id="sendMessage" class="btn btn-outline-success " >Send</button>
+                </div>
+                `
 };
 let ChannelPage=(id)=>{
   let   page=document.getElementById('page');
@@ -29,9 +40,41 @@ let ChannelPage=(id)=>{
           return response.json();
       })
       .then((data) => (page.innerHTML=html(data.channel.id,data.channel.title,data.channel.subject,data.channel.date,data.channel.state,data.channel.region,data.channel.user)))
+      .then(()=>{(document.getElementById('sendMessage')).addEventListener('click',send)}).then(()=>{listMessages(id)})
+      // .then(()=>{insertChat("me", "Hello Tom...", 0);  
+      // insertChat("you", "Hi, Pablo", 1500);
+      // insertChat("me", "What would you like to talk about today?", 3500);
+      // insertChat("you", "Tell me a joke",7000);
+      // insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 9500);
+      // insertChat("you", "LOL", 12000);
+      // })
       .catch((err) => onError(err));
       
     }
+   let  listMessages=(idChannel)=>{
+      fetch(API_URL+"channel/allMessages",{
+        method: "POST", 
+        body: JSON.stringify({idChannel}), 
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: getUserSessionData().token,
+        },}).then(res=>res.json())
+        .then(data=>{data.messages.forEach(e=>{insertChat(data.owner,e.username,e.message)})}).catch((err) => onError(err));
+    }
+    const send =(e)=>{
+      var message=document.getElementById('text-message').value;
+      let user=getUserSessionData().username;
+      let idChannel= e.target.dataset.id;
+      console.log("verify id channel "+idChannel+" message : "+ message+" by User :"+user);
+      fetch(API_URL+"channel/addMessage",{
+        method: "POST", 
+        body: JSON.stringify({user,message,idChannel}), 
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: getUserSessionData().token,
+        },}).then(res=>res.json())
+        .then(data=>{data.messages.forEach(e=>{console.log(e.username);console.log(data.owner);insertChat(data.owner,e.username,e.message)})}).catch((err) => onError(err));
+      };
     const onError = (err) => {
         console.error("Homepage::onError:", err);
         let errorMessage = "Error";
