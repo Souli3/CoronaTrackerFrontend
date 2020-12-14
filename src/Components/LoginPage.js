3ff6a806a05944407a8df95432e3a12deaf12c5f
Hosam <hosamien@gmail.com>
2020-12-14 18:17:03 +0100

    Update LoginPage.js

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -12,7 +12,7 @@ let loginPage = `
             <h1>CoronaTracker</h1><br><br>
             <h2>Login Page</h2>
             <p>Veuillez vous connecter ou vous inscrire pour acceder a toutes les fonctionnalites du site.</p><br><br><br><br><br><br><br>
-            <p><i>Ce site est un blog utilise pour tracer les cas de covid en Belgique.</i><p>
+            <p><i>Ce site est un blog utilise pour traquer les cas de covid en Belgique.</i><p>
             
             
          </div>
